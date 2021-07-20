88bd28f5c5184fed1c7f679fa5e803f7dcc6044e
hooman <hesamyan@outlook.com>
2021-07-20 13:41:09 +0400

    added CORS stereotype

--- src/main/java/com/example/demouploader/controller/DemoUploaderController.java
+++ src/main/java/com/example/demouploader/controller/DemoUploaderController.java
@@ -3,14 +3,11 @@ package com.example.demouploader.controller;
 import com.example.demouploader.service.UploadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 
-
+@CrossOrigin
 @Controller
 @RequestMapping("api/v1")
 public class DemoUploaderController {
