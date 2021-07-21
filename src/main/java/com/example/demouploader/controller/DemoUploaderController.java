200c2d8148f3a7fcf011d4beaf53c7eb40424236
hooman <hesamyan@outlook.com>
2021-07-21 15:56:35 +0400

    added Swagger

--- src/main/java/com/example/demouploader/controller/DemoUploaderController.java
+++ src/main/java/com/example/demouploader/controller/DemoUploaderController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 
-@CrossOrigin
+@CrossOrigin(maxAge = 3600)
 @Controller
 @RequestMapping("api/v1")
 public class DemoUploaderController {
